ef2aa8e6a8c31d3d4775bf0414af63d7d692f931
pantha704 <prathamjaiswal204@gmail.com>
2024-09-06 19:20:58 +0530

    .

---
+++ apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -0,0 +1,11 @@
+import SendCard from "../../../components/SendCard";
+
+const Page = () => {
+  return (
+    <div className="w-full">
+      <SendCard />
+    </div>
+  );
+};
+
+export default Page;
